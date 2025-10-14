1ae11a2fd68eddef6fbf04c9b65b1c6a2979598d
Nisa Dost <i.n.dost38@gmail.com>
2025-10-14 16:01:50 +0300

    create project

---
+++ src/test/java/com/codexist/pinpoint/PinpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexist.pinpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
